ca8e160b38e8072c141179a8301f87e455cbed8b
Rahul-Parihar <rspaarihar@gmail.com>
2026-07-03 17:19:06 +0530

    fix: add output standalone for Docker build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
   images: {
@@ -11,5 +12,4 @@ const nextConfig = {
     ],
   },
 };
-
 module.exports = nextConfig;
